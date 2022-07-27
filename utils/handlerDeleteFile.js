e3cbddcbaca1bf6717180895dac8a5545ee81845
rommer <rommer@tuemprende.com>
2022-07-27 03:32:01 -0500

    se agrega un return si el parametro en null

--- utils/handlerDeleteFile.js
+++ utils/handlerDeleteFile.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports.handlerDeleteFile = (pathFile) => {
+  if (!pathFile) return
+
   const cd = path.join(__dirname, '../')
   if (fs.existsSync(`${cd}${pathFile}`)) {
     fs.unlinkSync(`${cd}${pathFile}`)
