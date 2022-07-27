3dd4dde2878cd4fc427238b7fd49e592f6545179
rommer <rommer@tuemprende.com>
2022-07-27 00:17:14 -0500

    se usan dependencias cors y fileUpload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const path = require('path')
 const createError = require('http-errors')
 const connectionDB = require('./config/connectionDB')
@@ -9,9 +11,12 @@ const app = express()
 const routes = require('./routes/index')
 const { error404, handlerErrors } = require('./middleware/errors')
 
+// middlewares
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(fileUpload())
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes)
