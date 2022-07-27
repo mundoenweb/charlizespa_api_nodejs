1959cea74f434295a7f43c276ab44775bc5fdad2
rommer <rommer@tuemprende.com>
2022-07-27 00:19:46 -0500

    se crean nuevas rutas de consulta

--- apiServices/trataments/tratamentsRouter.js
+++ apiServices/trataments/tratamentsRouter.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { queryAllTrataments } = require('./controller')
+const {
+    getAllTrataments,
+    getAllTratamentsWeb,
+    getATratamentById
+} = require('./controller')
 
-router.get('/', queryAllTrataments)
+router.get('/', getAllTrataments)
+router.get('/web', getAllTratamentsWeb)
+router.get('/:id', getATratamentById)
+
+router.post('/')
+
 
 module.exports = router
