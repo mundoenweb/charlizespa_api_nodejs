a080485b5d74474aee73715eb9ba3101f1c25e56
rommer <rommer@tuemprende.com>
2022-07-27 00:19:27 -0500

    se renombra y mejora controlador para consultar todos los tratamientos, se crea controlador para consular los tratamientos vista admin, se crea controlador para consultar un solo tratamiento

--- apiServices/trataments/controller.js
+++ apiServices/trataments/controller.js
@@ -1,20 +1,49 @@
 const createError = require('http-errors')
-const { allServices } = require('./model')
+const { handlerResponse } = require('../../utils/handlerResponse')
+const {
+  allTrataments,
+  oneTratamentById
+} = require('./tratamentsModel')
 
-const queryAllTrataments =  (req, res, next) => {
-    
-    allServices()
-
-    .then(services => {
-        res.status(200)
-        res.json(services)
+const getAllTrataments = (req, res, next) => {
+  allTrataments()
+    .then(trataments => {
+      handlerResponse(res, trataments, 200)
     })
-    
     .catch(err => {
-        next(createError(500, err))
+      next(createError(500, err))
     })
 }
 
+const getAllTratamentsWeb = (req, res, next) => {
+  allTrataments('web')
+    .then(trataments => {
+      handlerResponse(res, trataments, 200)
+    })
+    .catch(err => {
+      next(createError(500, err))
+    })
+}
+const getATratamentById = (req, res, next) => {
+
+  const id = parseInt(req.params.id, 10)
+  if (!id) next(createError(400, 'favor pase un id valido en la url'))
+
+  oneTratamentById(id)
+    .then(tratament => {
+      handlerResponse(res, tratament, 200)
+    })
+    .catch(err => {
+      next(createError(500, err))
+    })
+}
+
+const postCreateTratament = (req, res, next) => {
+  
+}
+
 module.exports = {
-    queryAllTrataments
+  getAllTrataments,
+  getAllTratamentsWeb,
+  getATratamentById
 }
