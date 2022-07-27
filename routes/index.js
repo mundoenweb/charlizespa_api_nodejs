85394e1b0463e1da91247ec291fe1713b7ac6fbd
rommer <rommer@tuemprende.com>
2022-07-27 06:17:50 -0500

    se crea ruta public para servir los archivos estaticos del servidor

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const tratamentsRouter = require('../apiServices/trataments/tratamentsRouter')
 const servicesRouter = require('../apiServices/services/servicesRouter')
+const serveFileRouter = require('../apiServices/serveFiles/serveFilesRoutes')
 
 router.use('/trataments', tratamentsRouter)
 router.use('/services', servicesRouter)
+router.use('/public', serveFileRouter)
 
 module.exports = router
