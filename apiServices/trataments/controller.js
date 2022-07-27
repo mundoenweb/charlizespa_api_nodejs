0a49336f934d465f39ab24d8e3299306876d42fb
rommer <rommer@tuemprende.com>
2022-07-27 01:57:06 -0500

    se crea controlador para crear tratamiento

--- apiServices/trataments/controller.js
+++ apiServices/trataments/controller.js
@@ -1,9 +1,12 @@
 const createError = require('http-errors')
+const { handlerMoveImage } = require('../../utils/handlerMoveImage')
 const { handlerResponse } = require('../../utils/handlerResponse')
 const {
   allTrataments,
-  oneTratamentById
+  oneTratamentById,
+  createTratament
 } = require('./tratamentsModel')
+const { handlerDataCreateTratament } = require('./utils/handlerData')
 
 const getAllTrataments = (req, res, next) => {
   allTrataments()
@@ -38,12 +41,31 @@ const getATratamentById = (req, res, next) => {
     })
 }
 
-const postCreateTratament = (req, res, next) => {
+const postCreateTratament = async (req, res, next) => {
+  let data = req.body
+  const image = req.files?.image
+
+  const errorData = await handlerDataCreateTratament(data, image)
+  if(errorData) return next(createError(400, errorData))
+
+  await handlerMoveImage(image, 'trataments')
+  
+  data.image = image.pathDB
+
+  createTratament(data)
+  .then(response => {
+    const msg = 'tratamiento creado con exito'
+    handlerResponse(res, response, 201, msg)
+  })
+  .catch(err => {
+    next(createError(500, err))
+  })
   
 }
 
 module.exports = {
   getAllTrataments,
   getAllTratamentsWeb,
-  getATratamentById
+  getATratamentById,
+  postCreateTratament
 }
