ad4787947314f1a82b0efa5789985dd987741a2f
rommer <rommer@tuemprende.com>
2022-07-27 00:17:41 -0500

    se crea handler para responder facilmente a los usuarios

---
+++ utils/handlerResponse.js
@@ -0,0 +1,13 @@
+const handlerResponse = (res, data, code, message = 'consulta exitosa') => {
+  const objectResponse = {
+    data,
+    message,
+    status: code
+  }
+  res.status(code)
+  res.json(objectResponse)
+}
+
+module.exports = {
+  handlerResponse
+}
