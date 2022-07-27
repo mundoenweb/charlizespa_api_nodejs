ce11b1663976abad4a035f432b90df530c357846
rommer <rommer@tuemprende.com>
2022-07-27 03:40:43 -0500

    se crean rutas iniciales para los servicios

---
+++ apiServices/services/servicesRouter.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/') // obtiene todos los servicios
+router.get('/web') // obtiene todos los servicios menos los activos
+router.get('/:id') //obtiene un servicio
+
+router.put('/:id') // actualiza un servicio
+router.post('/') // crea un servicio
+router.delete('/:id') // elimina un servicio
+
+
+module.exports = router
