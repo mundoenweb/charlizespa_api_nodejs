775113f419d5e61008fa09f53b2a5f7c20561e0d
rommer <rommer@tuemprende.com>
2022-08-02 13:51:50 -0500

    se reorganiza el codigo

--- utils/handlerResponse.js
+++ utils/handlerResponse.js
@@ -1,9 +1,13 @@
-const handlerResponse = (res, data = [], code, message = 'consulta exitosa') => {
+const handlerResponse = (
+  res, data = [], code, message = 'consulta exitosa'
+) => {
+  
   const objectResponse = {
     data,
     message,
     status: code
   }
+
   res.status(code)
   res.json(objectResponse)
 }
